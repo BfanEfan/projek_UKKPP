100c4dbc6a6cbda2b7f39541acae403cb294fff6
BfanEfan <reifanefan894@gmail.com>
2026-04-17 11:16:41 +0700

    yang ini ketinggalan guys

--- db_pps.sql
+++ db_pps.sql
@@ -24,13 +24,15 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `id_admin` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('ADMIN','BK','WALI_KELAS','GURU') NOT NULL DEFAULT 'GURU',
+  `role` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_admin`),
-  UNIQUE KEY `username` (`username`)
+  UNIQUE KEY `username` (`username`),
+  KEY `role` (`role`),
+  CONSTRAINT `FK1_role` FOREIGN KEY (`role`) REFERENCES `m_role` (`role`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table db_ukkpp_new.admin: ~5 rows (approximately)
+-- Dumping data for table db_ukkpp_new.admin: ~7 rows (approximately)
 DELETE FROM `admin`;
 INSERT INTO `admin` (`id_admin`, `username`, `password`, `role`, `created_at`) VALUES
 	(1, 'admin', '$2b$10$nt36wgdEANocXnuuYtU0LO9KpqxX6GiZvw1qU1dgcOTL4k5x7gTcW', 'ADMIN', '2026-04-15 00:38:37'),
@@ -98,6 +100,22 @@ INSERT INTO `m_pelanggaran` (`id_pelanggaran`, `kode_pelanggaran`, `jenis`, `des
 	(6, 'PT1', 'KETERLAMBATAN', 'Terlambat masuk', 'Push up', 2, '2026-04-15 07:38:37'),
 	(7, 'PT2', 'KETERLAMBATAN', 'Keluar tidak kembali', 'Push up', 5, '2026-04-15 07:38:37');
 
+-- Dumping structure for table db_ukkpp_new.m_role
+CREATE TABLE IF NOT EXISTS `m_role` (
+  `id_role` int NOT NULL AUTO_INCREMENT,
+  `role` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_role`),
+  KEY `role` (`role`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table db_ukkpp_new.m_role: ~4 rows (approximately)
+DELETE FROM `m_role`;
+INSERT INTO `m_role` (`id_role`, `role`) VALUES
+	(1, 'ADMIN'),
+	(2, 'BK'),
+	(3, 'GURU'),
+	(4, 'WALI_KELAS');
+
 -- Dumping structure for table db_ukkpp_new.m_siswa
 CREATE TABLE IF NOT EXISTS `m_siswa` (
   `id_siswa` int NOT NULL AUTO_INCREMENT,
@@ -121,8 +139,14 @@ INSERT INTO `m_siswa` (`id_siswa`, `nipd`, `nama_siswa`, `kelas`, `jurusan`, `cr
 	(2, '2334104', 'candra', '11', 'TKJ 1', '2026-04-15 07:38:37'),
 	(3, '2334105', 'sipa', '10', 'BIDI 1', '2026-04-15 07:38:37'),
 	(4, '2334106', 'mutia', '12', 'RPL', '2026-04-15 07:38:37'),
-	(5, '2334107', 'noval', '12', 'DKV', '2026-04-15 07:38:37'),
-	(6, '2334108', 'Uki', '11', 'DKV', NULL);
+	(6, '2334108', 'Uki', '11', 'DKV', '2026-04-17 07:35:38'),
+	(7, '232410066', 'ahmad', '12', 'DKV', NULL),
+	(9, '232410012', 'mahmud', '12', 'TKJ 2', NULL),
+	(10, '232410031', 'karim', '11', 'TKJ 1', NULL),
+	(11, '232410087', 'aan', '10', 'DKV', NULL),
+	(12, '232410082', 'dika', '12', 'RPL', NULL),
+	(13, '232410083', 'dapit', '11', 'TKJ 1', NULL),
+	(14, '232410084', 'sapri', '10', 'DKV', NULL);
 
 -- Dumping structure for table db_ukkpp_new.pelanggaran_siswa
 CREATE TABLE IF NOT EXISTS `pelanggaran_siswa` (
@@ -139,9 +163,9 @@ CREATE TABLE IF NOT EXISTS `pelanggaran_siswa` (
   CONSTRAINT `pelanggaran_siswa_ibfk_1` FOREIGN KEY (`nipd`) REFERENCES `m_siswa` (`nipd`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `pelanggaran_siswa_ibfk_2` FOREIGN KEY (`id_pelanggaran`) REFERENCES `m_pelanggaran` (`id_pelanggaran`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `pelanggaran_siswa_ibfk_3` FOREIGN KEY (`created_by`) REFERENCES `admin` (`id_admin`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table db_ukkpp_new.pelanggaran_siswa: ~7 rows (approximately)
+-- Dumping data for table db_ukkpp_new.pelanggaran_siswa: ~9 rows (approximately)
 DELETE FROM `pelanggaran_siswa`;
 INSERT INTO `pelanggaran_siswa` (`id`, `nipd`, `id_pelanggaran`, `created_by`, `tanggal`, `catatan`) VALUES
 	(1, '2334103', 1, 2, '2026-04-01', NULL),
@@ -150,7 +174,15 @@ INSERT INTO `pelanggaran_siswa` (`id`, `nipd`, `id_pelanggaran`, `created_by`, `
 	(4, '2334105', 4, 2, '2026-04-03', NULL),
 	(5, '2334106', 7, 3, '2026-04-04', NULL),
 	(6, '2334103', 4, 1, '2026-04-16', NULL),
-	(7, '2334107', 3, 1, '2026-04-16', NULL);
+	(9, '2334108', 5, 7, '2026-04-17', NULL),
+	(10, '232410066', 1, 5, '2026-04-17', NULL),
+	(11, '232410066', 7, 5, '2026-04-17', NULL),
+	(14, '232410012', 2, 5, '2026-04-17', NULL),
+	(15, '232410012', 5, 5, '2026-04-17', NULL),
+	(16, '232410031', 3, 5, '2026-04-17', NULL),
+	(17, '232410087', 4, 5, '2026-04-17', NULL),
+	(18, '232410087', 6, 5, '2026-04-17', NULL),
+	(19, '232410087', 2, 5, '2026-04-17', NULL);
 
 -- Dumping structure for view db_ukkpp_new.v_detail_pelanggaran
 -- Creating temporary table to overcome VIEW dependency errors
@@ -163,7 +195,7 @@ CREATE TABLE `v_detail_pelanggaran` (
 	`poin` INT(10) NOT NULL,
 	`tanggal` DATE NOT NULL,
 	`input_by` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`role` ENUM('ADMIN','BK','WALI_KELAS','GURU') NOT NULL COLLATE 'utf8mb4_0900_ai_ci'
+	`role` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_0900_ai_ci'
 ) ENGINE=MyISAM;
 
 -- Dumping structure for view db_ukkpp_new.v_total_poin
